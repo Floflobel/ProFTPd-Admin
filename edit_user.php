31057ddaa168654b447893d7884c0b29ae82cd87
Florian Bellencontre <fbellencontre@cosium.com>
2017-12-01 11:45:00 +0100

    bu

--- edit_user.php
+++ edit_user.php
@@ -169,7 +169,7 @@ if (empty($errormsg)) {
   $uid      = $user[$field_uid];
   $ugid     = $user[$field_ugid];
   $passwd   = '';
-  $homedir  = substr($field_homedir, strlen($cfg['default_homedir']));
+  $homedir  = substr($user[$field_homedir], strlen($cfg['default_homedir']));
   $default_homedir  = 	$cfg['default_homedir'];
   $shell    = $user[$field_shell];
   $title    = $user[$field_title];
