389ca9e9cb87d11780688de08c679bebff12947e
Florian Bellencontre <fbellencontre@cosium.com>
2017-12-04 16:04:47 +0100

    bu

--- add_user.php
+++ add_user.php
@@ -194,7 +194,7 @@ include ("includes/header.php");
             <div class="form-group">
               <label for="<?php echo $field_homedir; ?>" class="col-sm-4 control-label">Home directory (<?php echo $homedir; ?>)</label>
               <div class="controls col-sm-8">
-                <input type="text" class="form-control" id="<?php echo $field_homedir; ?>" name="<?php echo $field_homedir; ?>" value="" placeholder="Enter a home folder" pattern="<?php echo substr($cfg['homedir_regex']); ?>" required />
+                <input type="text" class="form-control" id="<?php echo $field_homedir; ?>" name="<?php echo $field_homedir; ?>" value="" placeholder="Enter a home folder" pattern="/^([^\.]|([^\.])\.[^\.])*$/" required />
               </div>
             </div>
             <!-- Real name -->
