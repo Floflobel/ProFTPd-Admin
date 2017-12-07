f1944ac4e08f714f4c5f383a99b9f919d1a13136
Florian Bellencontre <fbellencontre@cosium.com>
2017-12-07 14:43:13 +0100

    bu

--- add_user.php
+++ add_user.php
@@ -189,7 +189,9 @@ include ("includes/header.php");
               <label for="<?php echo $field_ugid; ?>" class="col-sm-4 control-label">Main group <font color="red">*</font></label>
               <div class="controls col-sm-8">
                 <select class="form-control multiselect" id="<?php echo $field_ugid; ?>" name="<?php echo $field_ugid; ?>" required>
+                <?php print_r($groups) ?>
                 <?php while (list($g_gid, $g_group) = each($groups)) { ?>
+                  <?php print_r($g_gid); print_r($g_group); ?>
                   <option value="<?php echo $g_gid; ?>" <?php if ($ugid == $g_gid) { echo 'selected="selected"'; } ?>><?php echo $g_group; ?></option>
                 <?php } ?>
                 </select>
